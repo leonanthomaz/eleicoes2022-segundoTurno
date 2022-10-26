836c336ecd74fac9513c4ecbc54e820abe2140f5
leonanthomaz <leonan.thomaz@gmail.com>
2022-10-26 16:23:12 -0300

    update - modal candidato

--- src/app/components/CandidatoModal/index.jsx
+++ src/app/components/CandidatoModal/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Modal from 'react-bootstrap/Modal'
 import * as CM from './CandidatoModalStyles'
+import { GrClose } from "react-icons/gr";
 
 import imgLula from '../../assets/img/lula.png'
 import imgBolsonaro from '../../assets/img/bolsonaro.png'
@@ -16,48 +17,47 @@ export const CandidatoModal = ({ candidato, handleCloseCandidato, showCandidato 
       onHide={handleCloseCandidato}
       style={{background: '#14141498'}}
      >
-      <Modal.Header closeButton>
-          <Modal.Title>
-          <h1 style={{
-          color: '#14141498', 
-          textAlign: 'center', 
-          marginTop: '20px',
-          lineHeight: '5rem',
-          fontWeight: 900
-          }}>{candidato.nome === "LULA" ? 'LULA' 
+    <CM.Container>
+      <CM.Header>
+          <GrClose 
+          style={{color: '#14141498', 
+          float: 'right', 
+          fontSize: '20px',
+          cursor: 'pointer'
+          }} onClick={handleCloseCandidato}
+          />
+
+        <CM.Title>{candidato.nome === "LULA" ? 'LULA' 
           : candidato.nome === "JAIR BOLSONARO" ? 'JAIR BOLSONARO'
-          : ''}</h1>
-          </Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <CM.Container>
-          <CM.Box>
-            <div className='header-info'>
-              <div className='header-img'>
+          : ''}</CM.Title>
+
+      </CM.Header>
+      <CM.Box>
+            <div className='left'>
+              <div className='left-img'>
                 <img 
-                  src={
-                    candidato.nome === "LULA" ? imgLula 
-                    : candidato.nome === "JAIR BOLSONARO" ? imgBolsonaro 
-                    : ''} alt={candidato.nome} width={150} 
-                  />
-                </div>
-                <h3>{candidato.numero}</h3>
-            </div>
-            <div className='header-info'>
-              <div className='header-h'>
-                  <h4>Partido (Coligação):</h4> 
+                src={
+                  candidato.nome === "LULA" ? imgLula 
+                  : candidato.nome === "JAIR BOLSONARO" ? imgBolsonaro 
+                  : ''} alt={`Imagem do candidato ${candidato.nome}.`} loading="lazy" width={150} 
+                />
+              </div>
+              <h3>{candidato.numero}</h3>
+              <div className='left-h'>
                   <h5>{candidato.partido}</h5>
               </div>
-              <div className='header-f'>
+            </div>
+            <div className='right'>
+              <div className='right-h'>
                 <h3>{candidato.porcentagem}%</h3>
-                Qtd de votos:<span>{Number(candidato.totalvotos).toLocaleString('pt-BR')}</span>
               </div>
-              <span>Situação: <span>{candidato.situacao === 'n' ? candidato.situacao : 'Indefinida'}</span></span> 
+              <div className='right-f'>
+                <span>Votos: {Number(candidato.totalvotos).toLocaleString('pt-BR')}</span>
+                <span>Situação: {candidato.situacao ? candidato.situacao : 'Indefinida'}</span> 
+              </div>
             </div>
           </CM.Box>
-        </CM.Container>  
-      </Modal.Body>
-
+      </CM.Container>  
       <Modal.Footer>
           Fonte: TSE
       </Modal.Footer>
